42570b900544f4134f6e689923945107a526e1f7
Sanjay Tourani <tourani@utexas.edu>
2013-08-21 10:42:05 -0500

    Updated example.js

--- example.js
+++ example.js
@@ -2,13 +2,13 @@ var Topsy = require('./lib/client');
 
 var topsy = new Topsy(process.argv[2]);
 
-// topsy.getAuthorInfo({"url": "http://twitter.com/justinbieber"}, function(error, result) {
-//     console.log(result);
-// });
+topsy.getAuthorInfo({"url": "http://twitter.com/justinbieber"}, function(error, result) {
+    console.log(result);
+});
 
-// topsy.getExperts({"q": "Javascript"}, function(error, result) {
-//     console.log(result);
-// });
+topsy.getExperts({"q": "Javascript"}, function(error, result) {
+    console.log(result);
+});
 
 topsy.getStats({"url": "http://topsy.com"}, function(error, result) {
     console.log(result);
